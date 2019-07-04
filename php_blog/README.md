49cb6a0024063bd56b08cf98b810655a96d108ad
Ansh Puvvada <43775333+crusader2000@users.noreply.github.com>
2019-07-04 20:20:31 +0530

    Add files via upload

---
+++ php_blog/README.md
@@ -0,0 +1,10 @@
+First start the local server
+sudo /opt/lampp/lampp start
+
+To get the source code
+subl /opt/lampp/htdocs/php_blog
+
+To run the site
+firefox http://localhost/php_blog/index.php
+
+Copy this folder in /opt/lampp/htdocs
---
+++ php_blog/list-posts.php
@@ -0,0 +1,79 @@
+ <?php
+require_once 'lib/common.php';
+require_once 'lib/list-posts.php';
+
+session_start();
+
+if(!isLoggedIn()){
+    redirectAndExit('index.php');
+}
+
+if ($_POST)
+{
+    $deleteResponse = $_POST['delete-post'];
+    if ($deleteResponse)
+    {
+        $keys = array_keys($deleteResponse);
+        $deletePostId = $keys[0];
+        if ($deletePostId)
+        {
+            deletePost(getPDO(), $deletePostId);
+            redirectAndExit('list-posts.php');
+        }
+    }
+}
+
+$pdo=getPDO();
+$posts=getAllPosts($pdo);
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>A blog application | Blog posts</title>
+        <?php require 'templates/head.php' ?>
+    </head>
+    <body>
+        <?php require 'templates/top-menu.php' ?>
+
+        <h1>Post list</h1>
+<?php echo count($posts) ?>
+        <form method="post">
+            <table id="post-list">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Creation date</th>
+                        <th>Comments</th>
+                        <th />
+                        <th />
+                    </tr>
+</thead>
+                <tbody>
+                                  <?php foreach ($posts as $post): ?>
+                        <tr>
+                            <td>
+                            <a  href="view-post.php?post_id=<?php echo $post['id']?>">
+                            <?php echo htmlEscape($post['title']) ?>
+                            </td>
+                            <td>
+                                <?php echo convertSqlDate($post['created_at']) ?>
+                            </td>
+                            <td><?php echo $post['comment_count'] ?></td>
+                            <td>
+                                <a href="edit-post.php?post_id=<?php echo $post['id']?>">Edit</a>
+                            </td>
+                            <td>
+                                <input
+                                    type="submit"
+                                    name="delete-post[<?php echo $post['id']?>]"
+                                    value="Delete"
+                                />
+                            </td>
+                        </tr>
+                    <?php endforeach ?>
+                </tbody>
+            </table>
+        </form>
+    </body>
+</html>
